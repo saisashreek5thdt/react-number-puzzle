f935f1bc4ba4ac111b3b0dd4165916fa5c27586d
Sai Sashreek D <sai@5thdt.com>
2024-07-16 12:50:04 +0530

    Updated with Class Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Range, Every, Shuffle } from "react-lodash";
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const layout = (
+  <Range
+    start={0}
+    end={16}
+    step={(n) => {
+      const row = Math.floor(n / 4);
+      const col = n % 4;
+      return [80 * col, 80 * row];
+    }}
+  />
+);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      positions: <Shuffle collection={<Range start={0} end={16} />} />,
+    };
+  }
+  updatePosition(index) {
+    let { positions } = this.state;
+    let emptyIndex = positions.indexOf(0);
+    let targetIndex = positions.indexOf(index);
+    const dif = Math.abs(targetIndex - emptyIndex);
+    if (dif == 1 || dif == 4) {
+      positions[emptyIndex] = index;
+      positions[targetIndex] = 0;
+      this.setState({ positions });
+      let win = (
+        <Every
+          collection={positions}
+          predicate={(value, index, array) => {
+            value = value || 16;
+            return index === 0 || parseInt(array[index - 1]) <= parseInt(value);
+          }}
+        />
+      );
+      if (win) {
+        window.alert("U Win!!!");
+      }
+    }
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="game">
+          {this.state.positions.map((key) => {
+            let cellClass = key ? "cell" : "empty cell";
+            let [x, y] = layout[this.state.positions.indexOf(key)];
+            return (
+              <div
+                key={key}
+                className={cellClass}
+                onClick={this.updatePosition.bind(this, key)}
+                style={{ transform: `translate3d(${x}px,${y}px,0) scale(1.1)` }}
+              >
+                {key}
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
-
-export default App
